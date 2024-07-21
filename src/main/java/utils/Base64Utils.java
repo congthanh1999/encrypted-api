b8b2fa7cb1d4a6089ffc9eca8ff6055e7187c746
congthanh1999 <congthanh10021999@gmail.com>
2024-07-21 12:07:27 +0700

    implement a function to check if the string is base64 encoded

--- src/main/java/utils/Base64Utils.java
+++ src/main/java/utils/Base64Utils.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.regex.Pattern;
+
 public class Base64Utils {
     public static String encode(byte[] data) {
         return java.util.Base64.getEncoder().encodeToString(data);
@@ -7,5 +9,11 @@ public class Base64Utils {
 
     public static byte[] decode(String data) {
         return java.util.Base64.getDecoder().decode(data);
+    }
+
+    public static boolean isBase64Encoded (String message) {
+        Pattern base64Pattern = Pattern.compile("^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$");
+
+        return base64Pattern.matcher(message).matches();
     }
 }
