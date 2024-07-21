1b148040d15aa7b96794ffbcd8ebb88ee242d0ef
congthanh1999 <congthanh10021999@gmail.com>
2024-07-21 12:09:56 +0700

    add CRUD functions for a complete api

--- src/main/java/com/peter/encrypted_api/data_encryption/DataEncryptionController.java
+++ src/main/java/com/peter/encrypted_api/data_encryption/DataEncryptionController.java
@@ -2,10 +2,9 @@ package com.peter.encrypted_api.data_encryption;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -19,8 +18,22 @@ public class DataEncryptionController {
 
     @PostMapping("/encrypted_data")
     public ResponseEntity<Object> createDataEncryption(@RequestBody DataEncryption dataEncryption) {
-        dataEncryption.setContent(dataEncryption.encryptDataContent());
         return dataEncryptionService.saveEncryptedData(dataEncryption);
+    }
+
+    @GetMapping("/encrypted_data")
+    public ResponseEntity<List<DataEncryption>> fetchDataEncryption() {
+        return dataEncryptionService.findAll();
+    }
+
+    @PatchMapping("/encrypted_data/{id}")
+    public ResponseEntity<Object> updateDataEncryption(@PathVariable("id") Long id) {
+        return dataEncryptionService.editEncryptedData(id);
+    }
+
+    @DeleteMapping("/encrypted_data/{id}")
+    public ResponseEntity<Object> deleteDataEncryption(@PathVariable("id") Long id) {
+        return dataEncryptionService.removeDataEncryption(id);
     }
 
     @PostMapping("/decrypted_data")
--- src/main/java/com/peter/encrypted_api/data_encryption/DataEncryptionService.java
+++ src/main/java/com/peter/encrypted_api/data_encryption/DataEncryptionService.java
@@ -5,6 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import jakarta.persistence.EntityNotFoundException;
+import utils.Base64Utils;
+
+import java.util.List;
+
 @Service
 public class DataEncryptionService {
     private final DataEncryptionRepository dataEncryptionRepository;
@@ -14,9 +19,41 @@ public class DataEncryptionService {
         this.dataEncryptionRepository = dataEncryptionRepository;
     }
 
+    public ResponseEntity<List<DataEncryption>> findAll() {
+        return new ResponseEntity<>(dataEncryptionRepository.findAll(), HttpStatus.OK);
+    }
+
     public ResponseEntity<Object> saveEncryptedData(DataEncryption dataEncryption) {
+        dataEncryption.setContent(dataEncryption.encryptDataContent());
         dataEncryptionRepository.save(dataEncryption);
         return new ResponseEntity<>(dataEncryption, HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<Object> editEncryptedData(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("ID cannot be null");
+        }
+
+        DataEncryption existingDataEncryption = dataEncryptionRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException(String.valueOf(id)));
+
+        if (Base64Utils.isBase64Encoded(existingDataEncryption.getContent())) { // Successful decoding, content was Base64 encoded
+            existingDataEncryption.setContent(existingDataEncryption.decryptDataContent());
+        } else {
+            existingDataEncryption.setContent(existingDataEncryption.encryptDataContent());
+        }
+
+        return new ResponseEntity<>(dataEncryptionRepository.save(existingDataEncryption), HttpStatus.OK);
+    }
+
+    public ResponseEntity<Object> removeDataEncryption(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("ID cannot be null");
+        }
+
+        dataEncryptionRepository.deleteById(id);
+
+        return new ResponseEntity<>("Deleted encrypted data with id " + id, HttpStatus.OK);
     }
 
     public ResponseEntity<Object> saveDecryptedData(DataEncryption dataEncryption) {
