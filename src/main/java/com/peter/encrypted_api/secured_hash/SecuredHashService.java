61a3fc18a2d87b5cc0a4f6acaac6736c875032af
congthanh1999 <congthanh10021999@gmail.com>
2024-07-21 12:08:14 +0700

    implement function for fetching all hashed data for /api/v1/secured_hash

--- src/main/java/com/peter/encrypted_api/secured_hash/SecuredHashService.java
+++ src/main/java/com/peter/encrypted_api/secured_hash/SecuredHashService.java
@@ -1,10 +1,13 @@
 package com.peter.encrypted_api.secured_hash;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SecuredHashService {
@@ -19,5 +22,9 @@ public class SecuredHashService {
     public ResponseEntity<Object> saveSecuredHash(SecuredHash securedHash) {
         securedHashRepository.save(securedHash);
         return new ResponseEntity<>(securedHash, HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<SecuredHash>> findAllHashedData() {
+        return new ResponseEntity<>(securedHashRepository.findAll(), HttpStatus.OK);
     }
 }
